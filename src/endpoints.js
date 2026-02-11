3f2df322486a8ab36e84c0d2d55895fdce7c1a19
andrey <abdalovandrey95@gmail.com>
2026-02-11 11:26:54 -0800

    Create endpoints.js

---
+++ src/endpoints.js
@@ -0,0 +1,12 @@
+export const ENDPOINTS = {
+    general: 'https://68e839b2f2707e6128ca35e1.mockapi.io/chat',
+    photos: 'https://68eeed86b06cc802829ba196.mockapi.io/photos',
+    code: 'https://68eeed86b06cc802829ba196.mockapi.io/code',
+    players: 'https://68e839b2f2707e6128ca35e1.mockapi.io/players'
+};
+
+export const CHANNELS = [
+    { id: 'general', name: 'general', type: 'text', icon: 'Hash' },
+    { id: 'photos', name: 'photos', type: 'media', icon: 'Image' },
+    { id: 'code', name: 'code', type: 'code', icon: 'Code' }
+];
